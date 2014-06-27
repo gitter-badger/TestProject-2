4beb4f1ee1c1096d30820edc465cf4a434db8317
delmonto <admin@Mac-Admin.local>
2014-06-27 18:10:30 +0300

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Delmonto on 6/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Delmonto on 6/27/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
